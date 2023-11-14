7a5d67c8d38ebd429598f800f6b166e07d3fec01
arif <muhdarifrawi@gmail.com>
2023-11-14 20:01:08 +0800

    made changes to scroll button.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let pageArray = ["landing-page", "section-1", "section-2", "section-3", "section-4"];
 
 
-let downArrow = document.getElementsByClassName("down-arrow-icon")[0];
+//let downArrow = document.getElementsByClassName("down-arrow-icon")[0];
+let downArrow = $("#down-arrow-icon")[0];
 
-let num = 0
-downArrow.addEventListener("click", function(){
+let num = 1;
+
+downArrow.addEventListener("click", function () {
+
     let pageId = pageArray[num];
     console.log(pageId);
-    let y = document.getElementById(pageId).getBoundingClientRect().top;
-    window.scrollTo(0, y);
-    console.log(y);
-    if(num == 4){
+    document.location.href = "#" + pageId;
+    if (num == 4) {
         num = 0;
     }
     else {
         num += 1;
     }
 })
+
+
